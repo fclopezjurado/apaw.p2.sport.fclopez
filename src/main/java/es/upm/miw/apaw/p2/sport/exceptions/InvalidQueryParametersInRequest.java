7d19a943cae3fdc9ba7a56c9befee19a80eb4dab
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-19 22:40:58 +0200

    "InvalidQueryParametersInRequestException" abstract class implementation in "es.upm.miw.apaw.p2.sport.exceptions" package.
    This class is create to implement a protected method that must be used
    for exception classes in inheritance.

--- src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidQueryParametersInRequest.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidQueryParametersInRequest.java
@@ -1,5 +1,17 @@
 package es.upm.miw.apaw.p2.sport.exceptions;
 
-public abstract class InvalidQueryParametersInRequest {
+import java.util.Map;
 
+public abstract class InvalidQueryParametersInRequest {
+	protected String serializeRequestParameters(Map<String, String> requestParams) {
+		String serializedRequestParameters = new String("{");
+		for (Map.Entry<String, String> entry : requestParams.entrySet()) {
+			if (serializedRequestParameters.length() > 1)
+				serializedRequestParameters += ", \"";
+
+			serializedRequestParameters += entry.getKey() + "\": \"" + entry.getValue() + "\"";
+		}
+
+		return serializedRequestParameters + "}";
+	}
 }
