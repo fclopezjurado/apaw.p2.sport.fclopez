9d8d6577ed9eabc741b01610c109a4a5f5271aa7
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-19 23:31:10 +0200

    "InvalidQueryParametersInRequestException" class implementation in "es.upm.miw.apaw.p2.sport.exceptions" package using singleton pattern implemented in "SerializeMap" class from "es.upm.miw.apaw.p2.sport.aspects" package.

--- src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidQueryParametersInRequestException.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidQueryParametersInRequestException.java
@@ -3,24 +3,14 @@ package es.upm.miw.apaw.p2.sport.exceptions;
 import java.util.Map;
 import java.util.TreeMap;
 
+import es.upm.miw.apaw.p2.sport.aspects.SerializeMap;
+
 public class InvalidQueryParametersInRequestException extends Exception {
 	private static final long serialVersionUID = 938579940992492252L;
 	public static final String DESCRIPTION = "El nombre del parámetro introducido en la URI no es válido";
 
 	public InvalidQueryParametersInRequestException(Map<String, String> requestParams) {
-		super(DESCRIPTION + ". " + InvalidQueryParametersInRequestException.serializeRequestParameters(requestParams));
-	}
-
-	private static String serializeRequestParameters(Map<String, String> requestParams) {
-		String serializedRequestParameters = new String("{");
-		for (Map.Entry<String, String> entry : requestParams.entrySet()) {
-			if (serializedRequestParameters.length() > 1)
-				serializedRequestParameters += ", \"";
-
-			serializedRequestParameters += entry.getKey() + "\": \"" + entry.getValue() + "\"";
-		}
-
-		return serializedRequestParameters + "}";
+		super(DESCRIPTION + ". " + SerializeMap.getSerializeMap().mapSerialization(requestParams));
 	}
 
 	public InvalidQueryParametersInRequestException() {
