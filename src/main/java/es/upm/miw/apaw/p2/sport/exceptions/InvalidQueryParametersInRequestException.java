abc52e2f056e031d9a388914df96a3b34347362b
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-19 22:32:47 +0200

    "InvalidQueryParametersInRequestException" class implementation in "es.upm.miw.apaw.p2.sport.exceptions" package

--- src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidQueryParametersInRequestException.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidQueryParametersInRequestException.java
@@ -1,5 +1,30 @@
 package es.upm.miw.apaw.p2.sport.exceptions;
 
-public class InvalidQueryParametersInRequestException {
+import java.util.Map;
+import java.util.TreeMap;
+
+public class InvalidQueryParametersInRequestException extends Exception {
+	private static final long serialVersionUID = 938579940992492252L;
+	public static final String DESCRIPTION = "El nombre del parámetro introducido en la URI no es válido";
+
+	public InvalidQueryParametersInRequestException(Map<String, String> requestParams) {
+		super(DESCRIPTION + ". " + InvalidQueryParametersInRequestException.serializeRequestParameters(requestParams));
+	}
+
+	private static String serializeRequestParameters(Map<String, String> requestParams) {
+		String serializedRequestParameters = new String("{");
+		for (Map.Entry<String, String> entry : requestParams.entrySet()) {
+			if (serializedRequestParameters.length() > 1)
+				serializedRequestParameters += ", \"";
+
+			serializedRequestParameters += entry.getKey() + "\": \"" + entry.getValue() + "\"";
+		}
+
+		return serializedRequestParameters + "}";
+	}
+
+	public InvalidQueryParametersInRequestException() {
+		this(new TreeMap<String, String>());
+	}
 
 }
