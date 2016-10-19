4457cc17ae21ba9d2df9a5721f658f5cbb099e32
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-20 00:04:38 +0200

    "SportResource" class implementation in "es.upm.miw.apaw.p2.sport.api" package

--- src/main/java/es/upm/miw/apaw/p2/sport/api/SportResource.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/api/SportResource.java
@@ -1,5 +1,19 @@
 package es.upm.miw.apaw.p2.sport.api;
 
+import es.upm.miw.apaw.p2.sport.controllers.SportController;
+import es.upm.miw.apaw.p2.sport.exceptions.InvalidSportException;
+
 public class SportResource {
+
+	// POST **/sports body="sportName"
+	public void createSport(String sportName) throws InvalidSportException {
+		this.validateSport(sportName);
+		new SportController().createSport(sportName);
+	}
+
+	private void validateSport(String sportName) throws InvalidSportException {
+		if (SportController().isInvalidSport(sportName))
+			throw new InvalidSportException(sportName);
+	}
 
 }
