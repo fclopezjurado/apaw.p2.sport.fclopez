965a5a67a36af6341ef4a39d2ef28c0d920952c7
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-19 23:34:53 +0200

    "InvalidRequestBodyException" class implementation in "es.upm.miw.apaw.p2.sport.exceptions" package

--- src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidRequestBodyException.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidRequestBodyException.java
@@ -1,5 +1,15 @@
 package es.upm.miw.apaw.p2.sport.exceptions;
 
-public class InvalidRequestBodyException {
+public class InvalidRequestBodyException extends Exception {
+	private static final long serialVersionUID = 6626662996046832258L;
+	public static final String DESCRIPTION = "El contenido del cuerpo de la petición no es válido";
+
+	public InvalidRequestBodyException(String detail) {
+		super(DESCRIPTION + ". " + detail);
+	}
+
+	public InvalidRequestBodyException() {
+		this("");
+	}
 
 }
