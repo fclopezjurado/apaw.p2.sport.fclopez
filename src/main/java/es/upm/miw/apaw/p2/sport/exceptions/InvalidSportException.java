dea4b157eea3913ddf02a439c24f7b1c2c0e189b
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-20 00:56:21 +0200

    "InvalidSportException" class implementation in "es.upm.miw.apaw.p2.sport.exceptions" package

--- src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidSportException.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/exceptions/InvalidSportException.java
@@ -1,5 +1,15 @@
 package es.upm.miw.apaw.p2.sport.exceptions;
 
-public class InvalidSportException {
+public class InvalidSportException extends Exception {
+	private static final long serialVersionUID = 6150732997782365376L;
+	public static final String DESCRIPTION = "Deporte no válido";
+
+	public InvalidSportException(String detail) {
+		super(DESCRIPTION + ". " + detail);
+	}
+
+	public InvalidSportException() {
+		this("");
+	}
 
 }
