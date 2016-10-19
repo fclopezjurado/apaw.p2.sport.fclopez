b51db7551fc3e2fd8261aec8ed3cf117a7f358d5
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-20 01:43:17 +0200

    "CompleteUserListWrapper" class implementation in "es.upm.miw.apaw.p2.sport.wrappers" package

--- src/main/java/es/upm/miw/apaw/p2/sport/wrappers/CompleteUserListWrapper.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/wrappers/CompleteUserListWrapper.java
@@ -1,5 +1,30 @@
 package es.upm.miw.apaw.p2.sport.wrappers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompleteUserListWrapper {
+	private List<UserWrapper> completeUserList;
+
+	public CompleteUserListWrapper() {
+		this.completeUserList = new ArrayList<>();
+	}
+
+	public List<UserWrapper> getCompleteUserList() {
+		return this.completeUserList;
+	}
+
+	public void addUserWrapper(UserWrapper userWrapper) {
+		this.completeUserList.add(userWrapper);
+	}
+
+	@Override
+	public String toString() {
+		String result = "{\"completeUserList\":[ ";
+		for (UserWrapper userWrapper : this.completeUserList) {
+			result += userWrapper.toString() + ",";
+		}
+		return result.substring(0, result.length() - 1) + "]}";
+	}
 
 }
