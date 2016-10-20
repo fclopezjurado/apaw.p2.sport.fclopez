f1c0a1fb588eee7eacf2707ac2d90db5e10d5b8e
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-20 22:23:10 +0200

    "UserDao" interface definition in "es.upm.miw.apaw.p2.sport.daos" package

--- src/main/java/es/upm/miw/apaw/p2/sport/daos/UserDao.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/daos/UserDao.java
@@ -1,5 +1,14 @@
 package es.upm.miw.apaw.p2.sport.daos;
 
-public interface UserDao {
+import java.util.List;
+
+import es.upm.miw.apaw.p2.sport.entities.User;
+
+public interface UserDao extends GenericDao<User, String> {
+	boolean isInvalidUserNick(String userNick);
+
+	boolean isInvalidUserEmail(String userEmail);
+
+	List<Integer> findBySport(String sportName);
 
 }
