38793ba8630142a3ed6abc6a893253bb73429520
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-20 22:12:20 +0200

    "SportDao" interface definition in "es.upm.miw.apaw.p2.sport.daos" package

--- src/main/java/es/upm/miw/apaw/p2/sport/daos/SportDao.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/daos/SportDao.java
@@ -1,5 +1,10 @@
 package es.upm.miw.apaw.p2.sport.daos;
 
-public interface SportDao {
+import java.util.List;
+
+import es.upm.miw.apaw.p2.sport.entities.Sport;
+
+public interface SportDao extends GenericDao<Sport, String> {
+	boolean isInvalidSport(String sport);
 
 }
