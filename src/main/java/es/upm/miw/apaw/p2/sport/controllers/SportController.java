fa983295a1dbd3d26cbbbf18852bb39b90793c4e
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-20 20:17:22 +0200

    "SportController" class implementation in "es.upm.miw.apaw.p2.sport.controllers" package

--- src/main/java/es/upm/miw/apaw/p2/sport/controllers/SportController.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/controllers/SportController.java
@@ -1,5 +1,15 @@
 package es.upm.miw.apaw.p2.sport.controllers;
 
+import es.upm.miw.apaw.p2.sport.daos.DaoFactory;
+import es.upm.miw.apaw.p2.sport.entities.Sport;
+
 public class SportController {
 
+	public void createSport(String sportName) {
+		DaoFactory.getFactory().getSportDao().create(new Sport(sportName));
+	}
+	
+	public boolean isInvalidSport(String sportName) {
+		return DaoFactory.getFactory().getSportDao().isInvalidSport(sportName);
+	}
 }
