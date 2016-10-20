e4266fcb33d4c3a3ba6aeae604a65fced2040146
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-21 00:00:26 +0200

    "SportDaoMemory" class implementation in "es.upm.miw.apaw.p2.sport.daos.memory" package.

--- src/main/java/es/upm/miw/apaw/p2/sport/daos/memory/SportDaoMemory.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/daos/memory/SportDaoMemory.java
@@ -1,5 +1,28 @@
 package es.upm.miw.apaw.p2.sport.daos.memory;
 
-public class SportDaoMemory {
+import java.util.HashMap;
+
+import es.upm.miw.apaw.p2.sport.entities.Sport;
+import es.upm.miw.apaw.p2.sport.daos.SportDao;
+
+public class SportDaoMemory extends GenericMemoryDao<Sport> implements SportDao {
+
+	public SportDaoMemory() {
+		this.setMap(new HashMap<String, Sport>());
+	}
+	
+	@Override
+	protected String getId(Sport entity) {
+		return entity.getSportName();
+	}
+
+	@Override
+	public boolean isInvalidSport(String sportName) {
+		for (Sport sport: this.findAll())
+			if (sport.getSportName().equals(sportName))
+				return false;
+			
+		return true;
+	}
 
 }
