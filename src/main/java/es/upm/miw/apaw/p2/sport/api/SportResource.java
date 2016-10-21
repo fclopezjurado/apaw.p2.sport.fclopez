b87389b8a25a423bb695051e918a0f47a60e9fca
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-21 02:19:44 +0200

    Fixes in "SportResource" class from "es.upm.miw.apaw.p2.sport.api" package

--- src/main/java/es/upm/miw/apaw/p2/sport/api/SportResource.java
+++ src/main/java/es/upm/miw/apaw/p2/sport/api/SportResource.java
@@ -7,12 +7,13 @@ public class SportResource {
 
 	// POST **/sports body="sportName"
 	public void createSport(String sportName) throws InvalidSportException {
-		this.validateSport(sportName);
+		this.validateSport(sportName, false);
 		new SportController().createSport(sportName);
 	}
 
-	public void validateSport(String sportName) throws InvalidSportException {
-		if (new SportController().isInvalidSport(sportName))
+	public void validateSport(String sportName, boolean sportMustExist) throws InvalidSportException {
+		if ((new SportController().isInvalidSport(sportName) && !sportMustExist)
+				|| (!new SportController().isInvalidSport(sportName) && sportMustExist))
 			throw new InvalidSportException(sportName);
 	}
 
